17676cf5b142b9548bf5b3e1c62e9563a037994a
Jake Moe <JakesAddress@gmail.com>
2018-07-08 10:14:58 +1000

    * Changed to make BossBarVisible visible when elytra flying and hidden when not

--- src/main/java/com/gmail/jakesaddress/lolnetwings/LolnetWings.java
+++ src/main/java/com/gmail/jakesaddress/lolnetwings/LolnetWings.java
@@ -12,7 +12,7 @@ import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id="lolnetwings",
         name = "Lolnet Wings",
-        version = "0.4",
+        version = "0.5",
         description = "Lolnet Wings for Sponge")
 public class LolnetWings {
 
